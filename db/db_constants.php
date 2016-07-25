bd33a07f1d4ec58746d111516fdffc3541f79897
swaroop nagendra <swaroopnagendra76@gmail.com>
2016-07-25 19:48:34 +0530

    changed credentials

--- db/db_constants.php
+++ db/db_constants.php
@@ -1,8 +1,8 @@
 <?php
-	$host = "mysql.hostinger.in";
-	$username = "u488038781_swa";
-	$password = "REBECCA5922dylan";
-	$database = "u488038781_me2ng";
+	$host = hosting_server;
+	$username = username;
+	$password = password;
+	$database = db_name;
 
 	$conn = new mysqli($host, $username, $password, $database);
 	if ($conn->connect_error)
